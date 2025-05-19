df3eab0b044d4b73e5edbf484393412f5415326e
Stef Konings <2302771konings@zuyd.nl>
2025-05-19 11:03:52 +0200

    Nieuwe test klein order systeem

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+from order_system import OrderSystem
 
-print("Hello World");
+def main():
+    system = OrderSystem()
+    system.add_order("Order001", ["Patat", "Frikandel"])
+    system.view_orders()
+
+if __name__ == "__main__":
+    main()

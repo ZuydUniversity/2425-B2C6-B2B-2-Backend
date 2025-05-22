282c036916db8389f5a5ced2e2c6bee7e9e043f7
Roel Reijnders <2301988reijnders@zuyd.nl>
2025-05-22 13:23:34 +0200

    Fix merge conflict in Order.cs

--- backend/Models/Order.cs
+++ backend/Models/Order.cs
@@ -4,26 +4,17 @@ namespace backend.Models
 {
     public class Order
     {
-<<<<<<< HEAD
         public int Id { get; set; }
-        public string CustomerName { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
         public MotorType Type { get; set; }
         public int Quantity { get; set; }
-        public bool IsApproved { get; set; }
+        public bool IsApproved { get; set; } = false;
         public DateTime OrderDate { get; set; }
         public OrderStatus Status { get; set; }
 
-
-=======
-        public string CustomerName { get; set; } = string.Empty;
-        public int Quantity { get; set; }
-        public DateTime OrderDate { get; set; }
-        public bool IsApproved { get; set; } = false;
->>>>>>> f269250c88d0eb296690c44c960bc8c6408af5af
         public bool IsValid => Quantity >= 1 && Quantity <= 3;
-
-
     }
+
     public enum OrderStatus
     {
         New,
@@ -36,6 +27,4 @@ namespace backend.Models
         Shipped,
         Invoiced
     }
-
-
 }

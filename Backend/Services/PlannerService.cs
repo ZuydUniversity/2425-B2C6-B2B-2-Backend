990674a74ed59985f9b4f32dfe8514d63085a9df
Kasper Bakker <2305440bakker@zuyd.nl>
2025-05-25 18:45:20 +0200

    Planner en ProductionLine services toegevoegd en nodige regels code toegevoegd aan simulator.

---
+++ Backend/Services/PlannerService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Backend.Models;
+
+namespace Backend.Services
+{
+	public class PlannerService
+	{
+		public WorkOrder CreateWorkOrder(Order order, ProductionLine line)
+		{
+			return new WorkOrder
+			{
+				RelatedOrder = order,
+				OrderId = order.Id,
+				DeliveryDate = DateTime.Now.AddDays(7),
+				ProductionLine = line.Name
+			};
+		}
+
+		public void ReportToManagement()
+		{
+			Console.WriteLine("Planner rapporteert bezettingsgraad aan het management.");
+		}
+	}
+}

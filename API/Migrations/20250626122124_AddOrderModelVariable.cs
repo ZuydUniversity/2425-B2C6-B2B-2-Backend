efbaffe505d737e7e449095c64ce9a6e7605ac16
Roel Reijnders <2301988reijnders@zuyd.nl>
2025-06-26 14:22:06 +0200

    Nieuwe variable toegevoegd aan Order.

---
+++ API/Migrations/20250626122124_AddOrderModelVariable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderModelVariable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductionlineID",
+                table: "PurchaseOrders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PurchaseOrders_ProductionlineID",
+                table: "PurchaseOrders",
+                column: "ProductionlineID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PurchaseOrders_ProductionLines_ProductionlineID",
+                table: "PurchaseOrders",
+                column: "ProductionlineID",
+                principalTable: "ProductionLines",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PurchaseOrders_ProductionLines_ProductionlineID",
+                table: "PurchaseOrders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PurchaseOrders_ProductionlineID",
+                table: "PurchaseOrders");
+
+            migrationBuilder.DropColumn(
+                name: "ProductionlineID",
+                table: "PurchaseOrders");
+        }
+    }
+}

358e808f49dd0814eb802befababaf6fc2ef83ec
Macho <2304852hochstenbach@zuyd.nl>
2025-06-26 11:18:06 +0200

    OrdersController voor frontend aangepast.

---
+++ API/Dtos/OrderDto.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Dtos
+{
+    public class OrderDto
+    {
+        public int CustomerId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public string PicklistStatus { get; set; }
+        public string RejectionReason { get; set; }
+    }
+}
--- API/Dtos/OrderStatusDto.cs
+++ API/Dtos/OrderStatusDto.cs
@@ -4,6 +4,7 @@
     {
         public string Status { get; set; } = "Pending"; // Approved, Rejected, Delivered
         public string? Comment { get; set; } // Commentaar toevoegen voor terugkoppeling aan de klant.
+        public int CustomerId { get; set; }
     }
 }
 
